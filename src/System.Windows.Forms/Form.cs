14bb7d4de44506d47c4980f9076c0d41a453a5a9
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-11-19 02:45:43 +0100

    Set form size

--- src/System.Windows.Forms/Form.cs
+++ src/System.Windows.Forms/Form.cs
@@ -6,8 +6,13 @@ namespace System.Windows.Forms
     {
         public Form() : base()
         {
-            // Default form size
             Size = new Size(800, 600);
+        }
+
+        protected override void CreateHandle()
+        {
+            base.CreateHandle();
+            NativeMethods.QWidget_Resize(Handle, Size.Width, Size.Height);
         }
     }
 }
