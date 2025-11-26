7ac4074a45b898ed57b1fc37727bcf44e9ae6f88
alnkesq <141324281+alnkesq@users.noreply.github.com>
2025-11-26 21:42:10 +0100

    Add API surface

---
+++ src/System.Windows.Forms/SizeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Windows.Forms
+{
+    public enum SizeType
+    {
+        AutoSize,
+        Absolute,
+        Percent
+    }
+
+}
